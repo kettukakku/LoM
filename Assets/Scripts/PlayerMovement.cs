d896e3601698b9187e8d900dfca516b813d98314
Darien Kirst <darienkirst@gmail.com>
2021-01-24 15:48:20 -0500

    added basic player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class playerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Speed settings:")]
+    public float speed;
+    public float baseSpeed = 5.0f; //use this to augment speed for walking/running
 
-    // Update is called once per frame
+    private Vector2 moveDirection; //could make public to add status aligments like confuse that invert controls??
+
+    [Space]
+    [Header("References:")]
+    public Rigidbody2D rigidbody;
+    public Animator animator;
+
     void Update()
     {
-        
+        ProcessInputs(); 
+        Animate();  
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        speed = Mathf.Clamp(moveDirection.magnitude, 0.0f, 1.0f);
+        moveDirection.Normalize();
+    }
+
+    void Move()
+    {
+        rigidbody.velocity = moveDirection * speed * baseSpeed;
+    }
+
+    void Animate()
+    {
+        animator.SetFloat("Horizontal", moveDirection.x);
+        animator.SetFloat("Vertical", moveDirection.y);
     }
 }
