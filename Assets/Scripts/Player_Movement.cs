3c9bd637e023b4b3140f4cf508f46c430490579f
Darien Kirst <darienkirst@gmail.com>
2021-01-27 04:06:21 -0500

    oh christ okay the missing scripts should be resolved now. also redid the movement to be a little more terse.

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header ("Variables:")]
+    [SerializeField]
+    private float speed = 5f;
 
-    // Update is called once per frame
+    private Vector2 movement;
+  
+    [Header ("Components:")]
+    public Rigidbody2D rigidbody;
+    public Animator animator;
+
     void Update()
     {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movement.Normalize();
         
+       // movement.x = Input.GetAxisRaw("Horizontal");
+       // movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement != Vector2.zero){
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + movement * speed * Time.fixedDeltaTime);
     }
 }
